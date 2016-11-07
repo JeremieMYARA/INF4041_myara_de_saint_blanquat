a75e64297dcf3aa52f90c7b2ac306367c0a490c9
Jérémie <Jeremie@MacBook-Pro-de-Jeremie.local>
2016-11-07 16:42:38 +0100

    V2

--- app/src/main/java/org/esiea/myara_de_saint_blanquat/projet_mobile/MainActivity.java
+++ app/src/main/java/org/esiea/myara_de_saint_blanquat/projet_mobile/MainActivity.java
@@ -1,13 +1,55 @@
 package org.esiea.myara_de_saint_blanquat.projet_mobile;
 
+import android.app.DatePickerDialog;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private DatePickerDialog dpd_dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_button = (Button) findViewById(R.id.btn_button);
+        btn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Run", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final TextView  tv_Hello_World = (TextView) findViewById(R.id.tv_Hello_World);
+        tv_Hello_World.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello_World", Toast.LENGTH_LONG).show();
+                dpd_dialog.show();
+            }
+        });
+
+        dpd_dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                tv_Hello_World.setText("Date: " + day+ "/"+month+"/"+year);
+            }
+        },1985,11,30);
+
     }
 }
