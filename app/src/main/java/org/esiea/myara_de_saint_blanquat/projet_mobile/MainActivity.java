79ec24fbb1a67e6bc5b5eb2602f6cfa6002897c7
Jérémie <Jeremie@MacBook-Pro-de-Jeremie.local>
2016-12-14 10:49:00 +0100

    BUG sur AppCompatActivity

--- app/src/main/java/org/esiea/myara_de_saint_blanquat/projet_mobile/MainActivity.java
+++ app/src/main/java/org/esiea/myara_de_saint_blanquat/projet_mobile/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -21,6 +24,11 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -29,7 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     /**
@@ -38,16 +46,22 @@ public class MainActivity extends ActionBarActivity {
      */
     private DatePickerDialog dpd_dialog;
 
-    private  RecyclerView rv_biere;
+    private RecyclerView rv_biere;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         IntentFilter intentFilter = new IntentFilter(BIERS_UPDATE);
-        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(),intentFilter);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(), intentFilter);
         setContentView(R.layout.activity_main);
         Button btn_button = (Button) findViewById(R.id.btn_button);
         rv_biere = (RecyclerView) findViewById(R.id.rv_biere);
-        rv_biere.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        rv_biere.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         rv_biere.setAdapter(new BiersAdapter(getBiersFromFile()));
 
         btn_button.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +72,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        final TextView  tv_Hello_World = (TextView) findViewById(R.id.tv_Hello_World);
+        final TextView tv_Hello_World = (TextView) findViewById(R.id.tv_Hello_World);
         tv_Hello_World.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,20 +84,20 @@ public class MainActivity extends ActionBarActivity {
         dpd_dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                tv_Hello_World.setText("Date: " + day+ "/"+month+"/"+year);
+                tv_Hello_World.setText("Date: " + day + "/" + month + "/" + year);
             }
-        },1985,11,30);
+        }, 1985, 11, 30);
 
-        GetAllBiers.startActionGetAllBier(this);
-    }
+        GetAllBiers.startActionGetAllBier(this);}
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId()==R.id.search)
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.search)
             Toast.makeText(this, "search", Toast.LENGTH_LONG).show();
-        else if(item.getItemId()==R.id.test)
+        else if (item.getItemId() == R.id.btn_button)
             Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
         return true;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_2, menu);
@@ -91,19 +105,55 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-
     public static final String BIERS_UPDATE = "com.octip.cours.inf4042_11.BIERS_UPDATE";
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
     public class BierUpdate extends BroadcastReceiver {
-        public void onReceive(Context context, Intent intent){
+        public void onReceive(Context context, Intent intent) {
             Log.d(TAG, intent.getAction());
         }
     }
 
-    class BierReceiver extends BroadcastReceiver{
+    class BierReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            ((BiersAdapter)rv_biere.getAdapter()).setNewBiers(getBiersFromFile());
+            ((BiersAdapter) rv_biere.getAdapter()).setNewBiers(getBiersFromFile());
         }
     }
 
@@ -115,9 +165,10 @@ public class MainActivity extends ActionBarActivity {
             this.biers = biers;
         }
 
-        public void setNewBiers(JSONArray biers){
-            this.biers=biers;
+        public void setNewBiers(JSONArray biers) {
+            this.biers = biers;
         }
+
         public BierHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View itemView = LayoutInflater.from(getBaseContext()).inflate(R.layout.rv_bier_element, parent, false);
             return new BierHolder(itemView);
@@ -137,23 +188,26 @@ public class MainActivity extends ActionBarActivity {
             return biers.length();
         }
     }
-    public JSONArray getBiersFromFile(){
+
+    public JSONArray getBiersFromFile() {
         try {
             InputStream is = new FileInputStream(getCacheDir() + "/" + "bieres.json");
             byte[] buffer = new byte[is.available()];
             is.read(buffer);
             is.close();
             return new JSONArray(new String(buffer, "UTF-8"));
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return new JSONArray();
-        } catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
             return new JSONArray();
         }
     }
+
     public class BierHolder extends RecyclerView.ViewHolder {
         public TextView name;
+
         public BierHolder(View itemView) {
             super(itemView);
             name = (TextView) itemView.findViewById(R.id.rv_bier_element_name);
